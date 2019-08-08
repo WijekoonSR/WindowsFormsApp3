e44147cc7bc5bb2fecdc90d6bd05b79dc83359d5
WijekoonSR <42477588+WijekoonSR@users.noreply.github.com>
2019-08-08 20:37:22 +0530

    added employee ui user controls

--- WindowsFormsApp3/EmployeeUIEdit.Designer.cs
+++ WindowsFormsApp3/EmployeeUIEdit.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // EmployeeUIEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "EmployeeUIEdit";
+            this.Size = new System.Drawing.Size(1069, 648);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- WindowsFormsApp3/EmployeeUINew.Designer.cs
+++ WindowsFormsApp3/EmployeeUINew.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // EmployeeUINew
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "EmployeeUINew";
+            this.Size = new System.Drawing.Size(1069, 648);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
