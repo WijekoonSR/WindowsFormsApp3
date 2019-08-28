ce933bb1c69fbd6f9c44fefcf9a3d2b2f8373d68
Githma724 <53464334+Githma724@users.noreply.github.com>
2019-08-28 15:48:24 +0530

    test

--- WindowsFormsApp3/VehicleUINew.Designer.cs
+++ WindowsFormsApp3/VehicleUINew.Designer.cs
@@ -330,7 +330,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Goldenrod;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(462, 454);
+            this.button1.Location = new System.Drawing.Point(454, 454);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(77, 33);
             this.button1.TabIndex = 13;
