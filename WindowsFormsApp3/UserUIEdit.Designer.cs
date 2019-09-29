96cca835f5816eb0cae5c3e8a315bd5a232a7392
WijekoonSR <42477588+WijekoonSR@users.noreply.github.com>
2019-09-29 10:15:44 +0530

    fized error on user ui

--- WindowsFormsApp3/UserUIEdit.Designer.cs
+++ WindowsFormsApp3/UserUIEdit.Designer.cs
@@ -40,23 +40,22 @@
             this.lblEmployeeID = new System.Windows.Forms.Label();
             this.btnClear = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.picUser = new System.Windows.Forms.PictureBox();
             this.btnChangeUserPic = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.lblFileName = new System.Windows.Forms.Label();
             this.btnLoad = new System.Windows.Forms.Button();
             this.listView = new System.Windows.Forms.ListView();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picUser)).BeginInit();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(183, 374);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSave.Location = new System.Drawing.Point(137, 304);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(100, 28);
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
             this.btnSave.TabIndex = 32;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -64,59 +63,51 @@
             // 
             // txtReEnterPasword
             // 
-            this.txtReEnterPasword.Location = new System.Drawing.Point(183, 315);
-            this.txtReEnterPasword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtReEnterPasword.Location = new System.Drawing.Point(137, 256);
             this.txtReEnterPasword.Name = "txtReEnterPasword";
-            this.txtReEnterPasword.Size = new System.Drawing.Size(132, 22);
+            this.txtReEnterPasword.Size = new System.Drawing.Size(100, 20);
             this.txtReEnterPasword.TabIndex = 30;
             this.txtReEnterPasword.Visible = false;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(183, 267);
-            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPassword.Location = new System.Drawing.Point(137, 217);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.ReadOnly = true;
             this.txtPassword.Size = new System.Drawing.Size(100, 20);
-            this.txtPassword.ReadOnly = true;
-            this.txtPassword.Size = new System.Drawing.Size(132, 22);
             this.txtPassword.TabIndex = 29;
             // 
             // txtUserID
             // 
-            this.txtUserID.Location = new System.Drawing.Point(183, 226);
-            this.txtUserID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUserID.Location = new System.Drawing.Point(137, 184);
             this.txtUserID.Name = "txtUserID";
             this.txtUserID.ReadOnly = true;
-            this.txtUserID.Size = new System.Drawing.Size(132, 22);
+            this.txtUserID.Size = new System.Drawing.Size(100, 20);
             this.txtUserID.TabIndex = 28;
             // 
             // txtEmployeeID
             // 
-            this.txtEmployeeID.Location = new System.Drawing.Point(183, 180);
-            this.txtEmployeeID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtEmployeeID.Location = new System.Drawing.Point(137, 146);
             this.txtEmployeeID.Name = "txtEmployeeID";
             this.txtEmployeeID.ReadOnly = true;
-            this.txtEmployeeID.Size = new System.Drawing.Size(132, 22);
+            this.txtEmployeeID.Size = new System.Drawing.Size(100, 20);
             this.txtEmployeeID.TabIndex = 27;
             // 
             // lblUserID
             // 
             this.lblUserID.AutoSize = true;
-            this.lblUserID.Location = new System.Drawing.Point(23, 235);
-            this.lblUserID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUserID.Location = new System.Drawing.Point(17, 191);
             this.lblUserID.Name = "lblUserID";
-            this.lblUserID.Size = new System.Drawing.Size(55, 17);
+            this.lblUserID.Size = new System.Drawing.Size(43, 13);
             this.lblUserID.TabIndex = 25;
             this.lblUserID.Text = "User ID";
             // 
             // lbUsername
             // 
             this.lbUsername.AutoSize = true;
-            this.lbUsername.Location = new System.Drawing.Point(23, 324);
-            this.lbUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbUsername.Location = new System.Drawing.Point(17, 263);
             this.lbUsername.Name = "lbUsername";
-            this.lbUsername.Size = new System.Drawing.Size(130, 17);
+            this.lbUsername.Size = new System.Drawing.Size(98, 13);
             this.lbUsername.TabIndex = 24;
             this.lbUsername.Text = "Re-Enter Password";
             this.lbUsername.Visible = false;
@@ -124,93 +115,86 @@
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(23, 276);
-            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Location = new System.Drawing.Point(17, 224);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(69, 17);
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
             this.lblPassword.TabIndex = 23;
             this.lblPassword.Text = "Password";
             // 
             // lblEmployeeID
             // 
             this.lblEmployeeID.AutoSize = true;
-            this.lblEmployeeID.Location = new System.Drawing.Point(23, 188);
-            this.lblEmployeeID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEmployeeID.Location = new System.Drawing.Point(17, 153);
             this.lblEmployeeID.Name = "lblEmployeeID";
-            this.lblEmployeeID.Size = new System.Drawing.Size(87, 17);
+            this.lblEmployeeID.Size = new System.Drawing.Size(67, 13);
             this.lblEmployeeID.TabIndex = 22;
             this.lblEmployeeID.Text = "Employee ID";
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(53, 374);
-            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClear.Location = new System.Drawing.Point(40, 304);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(100, 28);
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
             this.btnClear.TabIndex = 31;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.btnChangeUserPic);
-            this.panel1.Location = new System.Drawing.Point(41, 4);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Location = new System.Drawing.Point(31, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(133, 134);
+            this.panel1.Size = new System.Drawing.Size(100, 109);
             this.panel1.TabIndex = 33;
-            // 
-            // picUser
-            // 
-            this.picUser.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.picUser.Image = ((System.Drawing.Image)(resources.GetObject("picUser.Image")));
-            this.picUser.Location = new System.Drawing.Point(0, 0);
-            this.picUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.picUser.Name = "picUser";
-            this.picUser.Size = new System.Drawing.Size(133, 106);
-            this.picUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picUser.TabIndex = 26;
-            this.picUser.TabStop = false;
-//            this.picUser.Click += new System.EventHandler(this.picUser_Click);
-            // 
-            // btnChangeUserPic
-            // 
-            this.btnChangeUserPic.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.btnChangeUserPic.Location = new System.Drawing.Point(0, 106);
-            this.btnChangeUserPic.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btnChangeUserPic.Name = "btnChangeUserPic";
-            this.btnChangeUserPic.Size = new System.Drawing.Size(133, 28);
-            this.btnChangeUserPic.TabIndex = 34;
-            this.btnChangeUserPic.Text = "Change";
-            this.btnChangeUserPic.UseVisualStyleBackColor = true;
-            this.btnChangeUserPic.Click += new System.EventHandler(this.btnChangeUserPic_Click);
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.picUser);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(100, 86);
             this.panel2.TabIndex = 35;
             // 
+            // picUser
+            // 
+            this.picUser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picUser.Image = ((System.Drawing.Image)(resources.GetObject("picUser.Image")));
+            this.picUser.Location = new System.Drawing.Point(0, 0);
+            this.picUser.Name = "picUser";
+            this.picUser.Size = new System.Drawing.Size(100, 86);
+            this.picUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picUser.TabIndex = 26;
+            this.picUser.TabStop = false;
+            // 
+            // btnChangeUserPic
+            // 
+            this.btnChangeUserPic.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnChangeUserPic.Location = new System.Drawing.Point(0, 86);
+            this.btnChangeUserPic.Name = "btnChangeUserPic";
+            this.btnChangeUserPic.Size = new System.Drawing.Size(100, 23);
+            this.btnChangeUserPic.TabIndex = 34;
+            this.btnChangeUserPic.Text = "Change";
+            this.btnChangeUserPic.UseVisualStyleBackColor = true;
+            this.btnChangeUserPic.Click += new System.EventHandler(this.btnChangeUserPic_Click);
+            // 
             // lblFileName
             // 
             this.lblFileName.AutoSize = true;
-            this.lblFileName.Location = new System.Drawing.Point(23, 156);
-            this.lblFileName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFileName.Location = new System.Drawing.Point(17, 127);
             this.lblFileName.Name = "lblFileName";
-            this.lblFileName.Size = new System.Drawing.Size(83, 17);
+            this.lblFileName.Size = new System.Drawing.Size(63, 13);
             this.lblFileName.TabIndex = 34;
             this.lblFileName.Text = "File Name ?";
             // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(124, 145);
-            this.btnLoad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLoad.Location = new System.Drawing.Point(93, 118);
             this.btnLoad.Name = "btnLoad";
-            this.btnLoad.Size = new System.Drawing.Size(93, 28);
+            this.btnLoad.Size = new System.Drawing.Size(70, 23);
             this.btnLoad.TabIndex = 35;
             this.btnLoad.Text = "Load";
             this.btnLoad.UseVisualStyleBackColor = true;
@@ -218,18 +202,17 @@
             // 
             // listView
             // 
+            this.listView.HideSelection = false;
             this.listView.Location = new System.Drawing.Point(137, 3);
-            this.listView.Location = new System.Drawing.Point(183, 4);
-            this.listView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(151, 133);
+            this.listView.Size = new System.Drawing.Size(114, 109);
             this.listView.TabIndex = 36;
             this.listView.UseCompatibleStateImageBehavior = false;
             this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
             // 
             // UserUIEdit
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.listView);
             this.Controls.Add(this.btnLoad);
@@ -245,12 +228,12 @@
             this.Controls.Add(this.lbUsername);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.lblEmployeeID);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "UserUIEdit";
-            this.Size = new System.Drawing.Size(254, 355);
+            this.Size = new System.Drawing.Size(272, 360);
+            this.Load += new System.EventHandler(this.UserUIEdit_Load);
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.picUser)).EndInit();
             this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picUser)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- WindowsFormsApp3/UserUIEdit.cs
+++ WindowsFormsApp3/UserUIEdit.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp3
 {
@@ -20,54 +21,124 @@ namespace WindowsFormsApp3
             InitializeComponent();
         }
 
+
         private void btnChangeUserPic_Click(object sender, EventArgs e)
         {
-            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "JPEG|*.jpg", ValidateNames = true, Multiselect = false}){
-                if (ofd.ShowDialog() == DialogResult.OK) {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "JPEG|*.jpg", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
                     filename = ofd.FileName;
                     lblFileName.Text = filename;
                     picUser.Image = Image.FromFile(filename);
                 }
-        }
+            }
         }
 
-        byte[] ConvertImageToBinary(Image img) {
-            using (MemoryStream ms = new MemoryStream()) {
+        byte[] ConvertImageToBinary(Image img)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
                 img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
                 return ms.ToArray();
             }
         }
         private void btnSave_Click(object sender, EventArgs e)
         {
-            using (BackhoeEntities db = new BackhoeEntities() ) {
-                User user = new User() { FileName = filename, UserPic = ConvertImageToBinary(picUser.Image) };//userpic in db
-                db.Users.Add(user);
-                db.SaveChangesAsync();
-                MessageBox.Show("Picture Saved","Success", MessageBoxButtons.OK);
-            }
+            /*using (BackhoeEntities db = new BackhoeEntities() ) {
+                 User user = new User() { FileName = filename, UserPic = ConvertImageToBinary(picUser.Image) };//userpic in db
+                 db.Users.Add(user);
+                 db.SaveChangesAsync();
+                 MessageBox.Show("Picture Saved","Success", MessageBoxButtons.OK);
+             }
+            */
+            String name = @"Data Source=(localDB)\Backhoe_DB;Initial Catalog=Backhoe;Integrated Security=True";
+            SqlConnection sql = new SqlConnection(name);
+            sql.Open();
+            string query = "insert into Users(UserPic) values(@id)";
+            SqlCommand sqlCommand = new SqlCommand(query, sql);
+            sqlCommand.Parameters.AddWithValue("@id", ConvertImageToBinary(picUser.Image));
+            sqlCommand.ExecuteNonQuery();
+            sql.Close();
         }
 
+        public static Bitmap ByteToImage(byte[] blob)
+        {
+            MemoryStream mStream = new MemoryStream();
+            byte[] pData = blob;
+            mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
+            Bitmap bm = new Bitmap(mStream, false);
+            mStream.Dispose();
+            return bm;
+        }
         private void btnLoad_Click(object sender, EventArgs e)
         {
             listView.Items.Clear();
-            using (BackhoeEntities DB = new BackhoeEntities()) {
+            using (BackhoeEntities DB = new BackhoeEntities())
+            {
                 list = DB.Users.ToList();
-                foreach(User user in list){
+                foreach (User user in list)
+                {
                     ListViewItem item = new ListViewItem(user.FileName);
                     listView.Items.Add(item);
                 }
             }
         }
-        Image ConvertBinaryToIamge(byte[] data) {
-            using (MemoryStream ms = new MemoryStream(data)) {
+        Image ConvertBinaryToIamge(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
                 return Image.FromStream(ms);
             }
         }
         private void listView_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (listView.FocusedItem != null) {
+            if (listView.FocusedItem != null)
+            {
                 picUser.Image = ConvertBinaryToIamge(list[listView.FocusedItem.Index].UserPic);
             }
         }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserUIEdit_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            String name = @"Data Source=(localDB)\Backhoe_DB;Initial Catalog=Backhoe;Integrated Security=True";
+
+            SqlConnection sql = new SqlConnection(name);
+            sql.Open();
+
+            String query = "select * from Users where UserID = 3";
+            SqlCommand sqlCommand = new SqlCommand(query, sql);
+
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+
+                // System.IO.MemoryStream ms = new System.IO.MemoryStream(byteBLOBData);
+                // picUser.Image = System.Drawing.Image.FromStream(ms);
+                //byte[] byteBLOBData = (byte[])reader["UserPic"];
+                //picUser.Image = ConvertBinaryToIamge(byteBLOBData);
+                byte[] byteBLOBData = (byte[])reader["UserPic"];
+                picUser.Image = ConvertBinaryToIamge(byteBLOBData);
+            }
+
+            sql.Close();
+        }
+
+        private void UserUIEdit_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+       
     }
 }
